cbf771031e085b999c97205dfb96dfba4f010af4
Jake Adams <jakeadams@SCs-MacBook-Pro-4.local>
2017-08-14 10:19:27 -0600

    CONNECTION: implemented getCon in order to get credentials from a file

--- CsvDb.pm
+++ CsvDb.pm
@@ -16,29 +16,64 @@ sub new {
 
 	my @columns = ();
 	my $error = "";
+    my $dbName = "";
+    my $dbHost = "";
+    my $dbUser = "";
+    my $dbPass = "";
 
 	$self->{_columns} = @columns;
 	$self->{_error} = $error;
+    $self->{_dbName} = $dbName;
+    $self->{_dbHost} = $dbHost;
+    $self->{_dbUser} = $dbUser;
+    $self->{_dbPass} = $dbPass;
+
 	return $self;
 }
 
-#TODO implement
-#"DBI:Pg:dbname=jacob;host=localhost", "postgres", "postgres"
 # $myConnection = DBI->connect("DBI:Pg:dbname=jacob;host=localhost", "postgres", "postgres")
 	# or die "DB connection error!";
-sub con {
+sub getCon {
 	my ($self) = @_;
 	my $dbName = "DBI:Pg:dbname=";
-	my $host = "host=";
-	my $user = "";
-	my $pass = "";
+	my $dbHost = "host=";
+	my $dbUser = "";
+	my $dbPass = "";
 
 	if(! -f $self->{_conFile}) {
 		$self->{_error} = "$self->{_conFile} does not exist!\n";
 		return 0;
 	}
 
-	#TODO read in file
+    open my $file, '<', $self->{_conFile};
+    my $conDataString = <$file>;
+    close $file;
+
+    my @conData = split(',', $conDataString);
+
+    #trim off whitespace
+    foreach (@conData){
+        $_ =~ s/^\s+|\s+$//g;
+    }
+
+    $dbName .= $conData[0];
+    $dbHost .= $conData[1];
+    $dbUser .= $conData[2];
+    $dbPass .= $conData[3];
+
+    $self->{_dbName} = $dbName;
+    $self->{_dbHost} = $dbHost;
+    $self->{_dbUser} = $dbUser;   
+    $self->{_dbPass} = $dbPass;
+
+}
+
+sub con{
+    my ($self) = @_;
+    print "$self->{_dbName}\n";
+    print "$self->{_dbHost}\n";
+    print "$self->{_dbUser}\n";
+    print "$self->{_dbPass}\n";
 }
 
 #TODO implement
@@ -47,6 +82,7 @@ sub readCsv {
 	print("$self->{_csvFile}\n");
 }
 
+# getters and setters
 sub getConFile {
     my ($self) = @_;
     return $self->{_conFile};
@@ -65,6 +101,46 @@ sub setCsvFile {
 sub getCsvFile {
     my ($self) = @_;
     return $self->{_csvFile};
+}
+
+sub setDbName {
+    my ($self, $dbName) = @_;
+    $self->{_dbName} = $dbName if defined($dbName);
+}
+
+sub getDbName {
+    my ($self) = @_;
+    return $self->{_dbname};
+}
+
+sub setDbHost {
+    my ($self, $dbHost) = @_;
+    $self->{_dbHost} = $dbHost if defined($dbHost);
+}
+
+sub getDbHost {
+    my ($self) = @_;
+    return $self->{_dbHost};
+}
+
+sub setDbUser {
+    my ($self, $dbUser) = @_;
+    $self->{_dbUser} = $dbUser if defined($dbUser);
+}
+
+sub getDbUser {
+    my ($self) = @_;
+    return $self->{_dbUser};
+}
+
+sub setDbPass {
+    my ($self, $dbPass) = @_;
+    $self->{_dbPass} = $dbPass if defined($dbPass);
+}
+
+sub getDbPass {
+    my ($self) = @_;
+    return $self->{_dbPass};
 }
 
 sub getError {
